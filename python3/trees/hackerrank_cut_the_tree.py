23645443e1a7ef3c097630edbda864e3177d1919
Maciej <maciekkra@gmail.com>
2022-01-29 14:12:41 +0100

    cut the tree solution

--- python3/trees/hackerrank_cut_the_tree.py
+++ python3/trees/hackerrank_cut_the_tree.py
@@ -2,65 +2,53 @@ from collections import defaultdict
 
 
 def cutTheTree(data, edges):
-    node_data = {i + 1: data[i] for i in range(len(data))}
+    def build_tree(edges):
+        tree = defaultdict(list)
+        for start, end in edges:
+            tree[start].append(end)
+            tree[end].append(start)
+        return tree
+
+    nodes = [i + 1 for i in range(len(data))]
+    tot_sum = sum(data)
     seen = set()
-    edges = [tuple(i) for i in edges]
-
-    def dfs(parent, tree):
-        final_sum = 0
-        seen.add(parent)
-        final_sum += node_data[parent]
-        for child in tree[parent]:
-            if child not in seen:
-                final_sum += dfs(child, tree)
-        return final_sum
-
+    stack = []
+    stack.append(nodes[0])
     tree = build_tree(edges)
-    tot_sum = dfs(1, tree)
-    diff = tot_sum
+    parent_child = dict()
+    nodes_weight = dict()
+    min_diff = None
 
-    for edge in edges:
-        p_1, p_2 = edge
-        # tree_1, tree_2 = build_trees(edges, [p_1, p_2])
-        tree_1 = build_trees(edges, [p_1, p_2])
-        seen.clear()
-        sum_1 = dfs(p_1, tree_1)
-        # seen.clear()
-        sum_2 = tot_sum - sum_1
-        diff = abs(sum_1 - sum_2) if diff == None else min(diff, abs(sum_1 - sum_2))
+    while stack:
+        parent = stack[-1]
 
-    return diff
+        if parent in seen:
+            nodes_weight[parent] = data[parent - 1]
+            for ch in tree[parent]:
+                if parent_child.get(ch, -1) == parent:
+                    nodes_weight[parent] += nodes_weight[ch]
 
+            sum_1 = nodes_weight[parent]
+            sum_2 = tot_sum - sum_1
+            min_diff = (
+                abs(sum_1 - sum_2)
+                if min_diff == None
+                else min(min_diff, abs(sum_1 - sum_2))
+            )
 
-def build_tree(edges):
-    tree = defaultdict(list)
-    for start, end in edges:
-        tree[start].append(end)
-        tree[end].append(start)
-    return tree
+            stack.pop()
+        seen.add(parent)
+        for ch in tree[parent]:
+            if ch not in seen:
+                stack.append(ch)
+                parent_child[ch] = parent
 
-
-def build_trees(edges, del_edge):
-    edges = [i for i in edges if i != tuple(del_edge)]
-    # print(f"{edges=}, {del_edge=}")
-
-    def find_edges(edges, parent):
-        found_edges = [i for i in edges if parent in i]
-        new_edges = list(set(edges) - set(found_edges))
-        new_vals = set([num for i in found_edges for num in i]) - set([parent])
-        for ch in new_vals:
-            found_edges += find_edges(new_edges, ch)
-        return found_edges
-
-    edges_1 = find_edges(edges, del_edge[0])
-    # edges_2 = find_edges(edges, del_edge[1])
-
-    return build_tree(edges_1)
-
-    # return build_tree(edges_1), build_tree(edges_2)
+    return min_diff
 
 
 data = [100, 200, 100, 500, 100, 600]
 edges = [[1, 2], [2, 3], [2, 5], [4, 5], [5, 6]]
+# data = [100, 200, 100, 500]
+# edges = [[1, 2], [2, 3], [2, 4]]
 
 cutTheTree(data, edges)
